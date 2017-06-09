92bc9341105f12acd62fa1798ce7c08e298c4230
dat17-05-ishiduki <175b005gp12@gmail.com>
2017-06-09 14:37:41 +0900

    if練習までの。。

--- move-lavels/Form1.cs
+++ move-lavels/Form1.cs
@@ -12,9 +12,30 @@ namespace move_lavels
 {
     public partial class Form1 : Form
     {
+        int vx = -10, vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left <= 0)
+                vx = -vx;
+            if (label1.Top <= 0)
+                vy = -vy;
+            if (label1.Left >= ClientSize.Width - label1.Width)
+                vx =-vx;
+            if (label1.Top >= ClientSize.Height - label1.Height)
+                vy = -vy;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
